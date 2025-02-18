4c0bc1eaf7655bb10beb27b72888f832fa7a7ef8
Vincent-odhiambo <oumavincentodhiambo12@gmail.com>
2025-02-18 09:11:12 +0300

    Add files via upload

---
+++ Porkcity/src/porkcity/Porkcity.java
@@ -0,0 +1,20 @@
+
+// or more specifically:
+
+
+package porkcity;
+
+
+public class Porkcity {
+
+
+    public static void main(String[] args) {
+        //OPEN THE SIGN UP PAGE TO COMMENCE THE PROJECT
+       Signup SignupFrame = new Signup();
+        SignupFrame.setVisible(true);
+        SignupFrame.pack();
+        SignupFrame.setLocationRelativeTo(null);
+        
+    }
+    
+}
